d118eec3cfaa19c46f3bf75d25ba5118e52bc7d0
Chris Edwards <cedwards@cedwards-2.local>
2016-07-13 15:45:16 -0400

    path fix

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,7 @@ var paths = {
     'test/mock/**/*.js',
     'test/spec/**/*.js'
   ],
-  karma: 'karma.conf.js',
+  karma: 'test/karma.conf.js',
   views: {
     main: yeoman.app + '/index.html',
     bowermain: yeoman.temp + '/index.html',
