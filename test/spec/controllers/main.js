9ba314f2ee307f1a44b0c75d997e66003e808637
Hardik Shah <hshah@reisystems.com>
2016-07-14 13:40:08 -0400

    Added karma

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,23 +1,28 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('MainCtrl', function () {
 
   // load the controller's module
   beforeEach(module('contactsApp'));
 
   var MainCtrl,
-    scope;
+    scope,
+    location;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootlocation, $rootScope) {
     scope = $rootScope.$new();
+    location=$rootlocation.$new();
     MainCtrl = $controller('MainCtrl', {
+      $location: location,
       $scope: scope
       // place here mocked dependencies
     });
   }));
 
   it('should attach a list of awesomeThings to the scope', function () {
-    expect(MainCtrl.awesomeThings.length).toBe(3);
+    console.log(MainCtrl);
+    MainCtrl.navigate('/test');
+    expect(MainCtrl.navigate()).toBe('/test');
   });
 });
